4f622039b34a0070c7b923df393468d6da7bbcb9
Mostafa Nemati <m.nemati.attar@gmail.com>
2025-12-12 23:27:12 +0330

    Add ApiGateway project and enhance UserService identity setup
    - Introduced Zentec.ApiGateway project with YARP reverse proxy, Dockerfile, and launch settings for .NET 8.
    - Updated solution file to include and nest ApiGateway under "src".
    - In Zentec.UserService:
      - Removed default WeatherForecast files.
      - Added Identity, EF Core, PostgreSQL, JwtBearer, and Serilog NuGet packages.
      - Created folder structure for Controllers, DTOs, Repositories, and Services.
      - Added ApplicationUser entity and UserDbContext for identity management.

---
+++ src/Zentec.UserService/Data/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Zentec.UserService.Models.Entities;
+
+namespace Zentec.UserService.Data
+{
+    public class UserDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+    }
+}
---
+++ src/Zentec.UserService/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Zentec.UserService.Models.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+    }
+}
